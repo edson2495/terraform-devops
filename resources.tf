12a2da1b897437a70da6d3e9548bbd2d7620638c
edson2495 <edson2495@hotmail.com>
2026-03-02 13:52:22 -0500

    Adding jenkins-nodes.tf

--- resources.tf
+++ resources.tf
@@ -1,43 +1,43 @@
-resource "aws_instance" "amazon_linux_2023" {
-  ami           = "ami-0320940581663281e"
-  instance_type = "t2.medium"
+# resource "aws_instance" "amazon_linux_2023" {
+#   ami           = "ami-0320940581663281e"
+#   instance_type = "t2.medium"
 
-  key_name = "terraform-keypair"
+#   key_name = "terraform-keypair"
 
-  vpc_security_group_ids = [aws_security_group.amazon_linux_sg.id]
+#   vpc_security_group_ids = [aws_security_group.amazon_linux_sg.id]
 
-  tags = {
-    Name = "amazon-linux-2023"
-  }
+#   tags = {
+#     Name = "amazon-linux-2023"
+#   }
 
-  root_block_device {
-    volume_size = 18
-    volume_type = "gp2"
-  }
-}
+#   root_block_device {
+#     volume_size = 18
+#     volume_type = "gp2"
+#   }
+# }
 
 
-resource "aws_security_group" "amazon_linux_sg" {
-  name = "amazon-linux-sg"
+# resource "aws_security_group" "amazon_linux_sg" {
+#   name = "amazon-linux-sg"
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   ingress {
+#     from_port   = 22
+#     to_port     = 22
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   ingress {
+#     from_port   = 8080
+#     to_port     = 8080
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+# }
